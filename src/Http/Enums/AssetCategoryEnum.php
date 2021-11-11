b625d893409047b0d1271550d9835c1c2e09ce9d
Lalita Wagh <lalita@riteserve.co>
2021-11-11 19:06:29 +0530

    create enums for other wallet configuration fiels

---
+++ src/Http/Enums/AssetCategoryEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Riteserve\LedgerFoundation\Http\Enums;
+
+use MyCLabs\Enum\Enum;
+
+class AssetCategoryEnum  extends Enum {
+   public const FIAT_CURRENCY = 'fiat_currency';
+   public const NON_FIAT_CURRENCY = 'non_fiat_currency';
+}
---
+++ src/Http/Enums/ExchangeTypeEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Riteserve\LedgerFoundation\Http\Enums;
+
+use MyCLabs\Enum\Enum;
+
+class ExchangeTypeEnum extends Enum {
+   public const FIAT = 'fiat';
+   public const NON_FIAT = 'non_fiat';
+
+}
