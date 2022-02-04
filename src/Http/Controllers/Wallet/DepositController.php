950f6233a6d6694a74c2b13074d123462ec772b3
Lalita <lalita@riteserve.co>
2022-02-04 16:09:34 +0530

    update seeder

--- src/Http/Controllers/Wallet/DepositController.php
+++ src/Http/Controllers/Wallet/DepositController.php
@@ -26,22 +26,33 @@ class DepositController extends Controller
 
     public function index(Request $request)
     {
-        $this->authorize(DepositPolicy::VIEW, Wallet::class);
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
 
-        $workspace = null;
+        if ($user->isSuperAdmin()) {
 
-        if ($request->has('filter.workspace_id')) {
-            $workspace = Workspace::findOrFail($request->input('filter.workspace_id'));
+            $transactions = Transaction::where("meta->transaction_type", 'deposit')->latest()->paginate();
+            return view("ledger-foundation::wallet.deposit.index", compact('transactions'));
+
+        } else {
+
+            $this->authorize(DepositPolicy::VIEW, Wallet::class);
+
+            $workspace = null;
+
+            if ($request->has('filter.workspace_id')) {
+                $workspace = Workspace::findOrFail($request->input('filter.workspace_id'));
+            }
+
+            $transactions = QueryBuilder::for(Transaction::class)
+                ->allowedFilters([
+                    AllowedFilter::exact('workspace_id'),
+                ]);
+
+            $transactions = Transaction::where("meta->transaction_type", 'deposit')->latest()->paginate();
+
+            return view("ledger-foundation::wallet.deposit.index", compact('workspace', 'transactions'));
         }
-
-        $transactions = QueryBuilder::for(Transaction::class)
-            ->allowedFilters([
-                AllowedFilter::exact('workspace_id'),
-            ]);
-
-        $transactions = Transaction::where("meta->transaction_type", 'deposit')->latest()->paginate();
-
-        return view("ledger-foundation::wallet.deposit.index", compact('workspace', 'transactions'));
     }
 
     public function create(Request $request)
