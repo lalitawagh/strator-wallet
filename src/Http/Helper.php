b3484a8ea187f91e8e41baabcd324edcd839e2e5
Lalita <lalita@riteserve.co>
2022-02-14 19:36:38 +0530

    update migration file

--- src/Http/Helper.php
+++ src/Http/Helper.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Collection;
 
 class Helper
 {
-    public static function paginate($items, $perPage = 2, $page = null, $options = [])
+    public static function paginate($items, $perPage = 7, $page = null, $options = [])
     {
         $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
         $items = $items instanceof Collection ? $items : Collection::make($items);
