8aa91ca108de7ab66bf2c84e7c6e82d215089c83
vivek <vivek@riteserve.co>
2022-04-08 14:44:13 +0530

    transaction

--- resources/views/wallet/transactions.blade.php
+++ resources/views/wallet/transactions.blade.php
@@ -35,7 +35,7 @@
                 </div>
                 <div class="p-5">
                     @if (\Illuminate\Support\Facades\Auth::user()->isSubscriber() && is_null($walletID))
-                        <div id="multiple-item-slider" class="wallet-slide preview" role="tablist">
+                        <div id="multiple-item-slider" class="wallet-slide preview pb-10" role="tablist">
                             <div class="mx-6">
                                 <div class="multiple-items">
                                     @foreach ($wallets as $key =>  $wallet)
