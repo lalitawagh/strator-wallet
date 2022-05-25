5fb717c3895aa38bb8edcf83c9ee7231a926084b
Chandan Panda <chandan@kanexy.com>
2022-05-25 12:06:44 +0000

    Updated assetclasstest.php

--- tests/Unit/assetclasstest.php
+++ tests/Unit/assetclasstest.php
@@ -10,6 +10,76 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+  public function index()
+    {
+        $products = Product::latest()->paginate(5);
+    
+        return view('products.index',compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+     
+   
+    public function create()
+    {
+        return view('products.create');
+    }
+    
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+    
+        Product::create($request->all());
+     
+        return redirect()->route('products.index')
+                        ->with('success','Product created successfully.');
+    }
+     
+    
+    public function show(Product $product)
+    {
+        return view('products.show',compact('product'));
+    } 
+     
+  
+    public function edit(Product $product)
+    {
+        return view('products.edit',compact('product'));
+    }
+    
+    
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+    
+        $product->update($request->all());
+    
+        return redirect()->route('products.index')
+                        ->with('success','Product updated successfully');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+    
+        return redirect()->route('products.index')
+                        ->with('success','Product deleted successfully');
+    }
+}
+
+
     public function setUp(): void
     {
         parent::setUp();
