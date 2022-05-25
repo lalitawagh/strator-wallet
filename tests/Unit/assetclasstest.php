e4a4b62e5a8cbfde2fd482f1e9e7043d976a2525
Chandan Panda <chandan@kanexy.com>
2022-05-25 10:56:06 +0000

    Updated assetclasstest.php

---
+++ tests/Unit/assetclasstest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kanexy\LedgerFoundation\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Kanexy\Cms\CmsServiceProvider;
+use Kanexy\PartnerFoundation\PartnerFoundationServiceProvider;
+use Livewire\LivewireServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Kanexy\\PartnerFoundation\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            PartnerFoundationServiceProvider::class,
+            LivewireServiceProvider::class,
+            CmsServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+
+        $app['config']->set('view.paths', [
+            __DIR__.'/views',
+            resource_path('views'),
+        ]);
+
+        $app['config']->set('app.key', 'base64:Hupx3yAySikrM2/edkZQNQHslgDWYfiBfCuSThJ5SK8=');
+
+        $app['config']->set('database.connections.mysql', [
+            'driver'   => 'mysql',
+            'database' => 'kanexyv1',
+            'prefix'   => '',
+            'host' => '127.0.0.1',
+            'username'=> 'root',
+            'password' => ''
+        ]);
+    }
+}
