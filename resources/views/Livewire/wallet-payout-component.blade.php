46dabe9356ff6650c99e867bb6fbf2b39de946b7
vivek tiwari <vivek@kanexy.com>
2022-06-23 14:53:50 +0530

    plus position change wallet form

--- resources/views/Livewire/wallet-payout-component.blade.php
+++ resources/views/Livewire/wallet-payout-component.blade.php
@@ -28,22 +28,25 @@
         </div>
     </div>
     <div class="grid grid-cols-12 md:gap-10 mt-0">
-        <div wire:ignore class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2 relative">
+        <div wire:ignore class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2">
             <label for="beneficiary" class="form-label sm:w-30"> Beneficiary <span
                     class="text-theme-6">*</span></label>
             <div class="sm:w-5/6 tillselect-marging">
-                <select wire:change="changeBeneficiary($event.target.value)" name="beneficiary" id="beneficiary" class="form-control"  data-search="true">
-                    @foreach ($beneficiaries as $beneficiary)
-                        <option value="{{ $beneficiary->getKey() }}">{{ $beneficiary->getFullName() }}</option>
-                    @endforeach
-                </select>
+                <div class="w-full relative">
+                    <select wire:change="changeBeneficiary($event.target.value)" name="beneficiary" id="beneficiary" class="form-control"  data-search="true">
+                        @foreach ($beneficiaries as $beneficiary)
+                            <option value="{{ $beneficiary->getKey() }}">{{ $beneficiary->getFullName() }}</option>
+                        @endforeach
+                    </select>
+                    <a data-toggle="modal" data-target="#walletbenificary-modal" class="absolute top-0 right-0 plus" style="">
+                        <i data-feather="plus-circle" class="w-4 h-4 ml-4"></i>
+                    </a>
+                </div>
                 @error('beneficiary')
                     <span class="block text-theme-6 mt-2">{{ $message }}</span>
                 @enderror
             </div>
-            <a data-toggle="modal" data-target="#walletbenificary-modal" class="absolute top-0 right-0 plus" style="">
-                <i data-feather="plus-circle" class="w-4 h-4 ml-4"></i>
-            </a>
+
         </div>
         <div class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2">
             <label for="phone" class="form-label sm:w-30"> Mobile</label>
--- resources/views/wallet/withdraw/create.blade.php
+++ resources/views/wallet/withdraw/create.blade.php
@@ -9,7 +9,7 @@
             <div class="box">
                 <div class="flex items-center p-3 border-b border-gray-200 dark:border-dark-5">
                     <h2 class="font-medium text-base mr-auto">
-                        Withdraw
+                        Withdraw1
                     </h2>
                 </div>
                 <div class="p-5">
