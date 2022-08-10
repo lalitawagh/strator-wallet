a53796a350c86a5becf1ceb02eeb939af1408326
vivek <vivek@kanexy.com>
2022-08-10 23:28:36 +0530

    updated changes

--- resources/views/asset-type/edit.blade.php
+++ resources/views/asset-type/edit.blade.php
@@ -1,86 +1,105 @@
-@extends("ledger-foundation::config-skeleton")
+@extends('ledger-foundation::config-skeleton')
 
-@section("title", "Edit Asset Type")
+@section('title', 'Edit Asset Type')
 
-@section("config-content")
+@section('config-content')
     <div class="configuration-container w-screen">
         <div class="grid grid-cols-12 gap-6">
             <!-- BEGIN: Daily Sales -->
             <div class="intro-y box col-span-12 xxl:col-span-12">
-                <div class="flex gap-2 sm:gap-0 flex-wrap items-center px-5 py-5 sm:py-3 border-b border-gray-200 dark:border-dark-5">
+                <div
+                    class="flex gap-2 sm:gap-0 flex-wrap items-center px-5 py-5 sm:py-3 border-b border-gray-200 dark:border-dark-5">
 
                     <div class="breadcrumb mr-auto hidden sm:flex">
-                        <a href="">Wallet</a><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                        <a href="" class="">Configuration</a><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                        <a href="">Wallet</a><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                        <a href="" class="">Configuration</a><svg xmlns="http://www.w3.org/2000/svg"
+                            width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
                         <a href="" class="breadcrumb--active">Edit Asset Type</a>
                     </div>
                 </div>
                 <div class="p-5">
-                    <form action="{{ route('dashboard.wallet.asset-type.update',$asset_type['id']) }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('dashboard.wallet.asset-type.update', $asset_type['id']) }}" method="POST"
+                        enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
-                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-2">
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-0">
                             <div class="col-span-12 md:col-span-8 xl:col-span-6 form-inline mt-2">
-                                <label for="asset_category" class="form-label sm:w-30">Asset Category <span class="text-theme-6">*</span></label>
+                                <label for="asset_category" class="form-label sm:w-30">Asset Category <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6 tillselect-marging">
                                     @php
                                         $asset_categories = \Kanexy\LedgerFoundation\Enums\AssetCategory::toArray();
                                     @endphp
-                                    <select name="asset_category" id="asset_category" data-search="true" class="tom-select w-full @error('asset_category') border-theme-6 @enderror">
+                                    <select name="asset_category" id="asset_category" data-search="true"
+                                        class="tom-select w-full @error('asset_category') border-theme-6 @enderror">
                                         @foreach ($asset_categories as $key => $asset_category)
-                                            <option value="{{ $asset_category }}" @if (old('asset_category',$asset_type['asset_category']) == $asset_category) selected @endif>{{ trans('ledger-foundation::configuration.'.$asset_category) }}</option>
+                                            <option value="{{ $asset_category }}"
+                                                @if (old('asset_category', $asset_type['asset_category']) == $asset_category) selected @endif>
+                                                {{ trans('ledger-foundation::configuration.' . $asset_category) }}</option>
                                         @endforeach
                                     </select>
 
                                     @error('asset_category')
-                                    <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
 
                             <div class="col-span-12 md:col-span-8 xl:col-span-6 form-inline mt-4">
-                                <label for="name" class="form-label sm:w-30">Name <span class="text-theme-6">*</span></label>
+                                <label for="name" class="form-label sm:w-30">Name <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6">
                                     <input id="name" name="name" type="text"
                                         class="form-control @error('name') border-theme-6 @enderror"
-                                        value="{{ old('name',$asset_type['name']) }}" required>
+                                        value="{{ old('name', $asset_type['name']) }}" required>
 
                                     @error('name')
-                                    <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
                         </div>
 
-                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-2">
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-0">
                             <div class="col-span-12 md:col-span-8 xl:col-span-6 form-inline mt-2">
                                 <label for="logo" class="form-label sm:w-30"> Image </label>
                                 <div class="sm:w-5/6">
                                     <input type="file" class="form-control" name="image">
                                     @isset($asset_type['image'])
-                                    <img class="rounded-md proof-default pt-2" style="width:100px;" alt="" src="{{ \Illuminate\Support\Facades\Storage::disk('azure')->temporaryUrl($asset_type['image'],now()->addMinutes(5)) }}">
+                                        <img class="rounded-md proof-default pt-2" style="width:100px;" alt=""
+                                            src="{{ \Illuminate\Support\Facades\Storage::disk('azure')->temporaryUrl($asset_type['image'], now()->addMinutes(5)) }}">
                                     @endisset
 
                                     @error('image')
-                                    <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
 
                             <div class="col-span-12 md:col-span-8 xl:col-span-6 form-inline mt-2">
                                 <label for="status" class="form-label sm:w-30"> Active</label>
-                                <div class="sm:w-5/6">
-                                    <input id="status" name="status" type="checkbox" class="form-check-input" @if (old("status",$asset_type['status'])  === \Kanexy\LedgerFoundation\Enums\WalletStatus::ACTIVE) checked @endif>
+                                <div class="sm:w-5/6 form-check form-switch">
+                                    <input id="status" name="status" type="checkbox" class="form-check-input"
+                                        @if (old('status', $asset_type['status']) === \Kanexy\LedgerFoundation\Enums\WalletStatus::ACTIVE) checked @endif>
 
                                     @error('status')
-                                    <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
                         </div>
 
                         <div class="text-right mt-5">
-                            <a href="{{ route('dashboard.wallet.asset-type.index') }}" class="btn btn-secondary w-24 inline-block mr-1">Cancel</a>
+                            <a href="{{ route('dashboard.wallet.asset-type.index') }}"
+                                class="btn btn-secondary w-24 inline-block mr-1">Cancel</a>
                             <button type="submit" class="btn btn-primary w-24">Update</button>
                         </div>
                     </form>
--- resources/views/master-account/create.blade.php
+++ resources/views/master-account/create.blade.php
@@ -1,30 +1,45 @@
-@extends("ledger-foundation::config-skeleton")
+@extends('ledger-foundation::config-skeleton')
 
-@section("title", "Create Master Account")
+@section('title', 'Create Master Account')
 
-@section("config-content")
+@section('config-content')
     <div class="configuration-container w-screen">
         <div class="grid grid-cols-12 gap-6">
             <!-- BEGIN: Daily Sales -->
             <div class="intro-y box col-span-12 xxl:col-span-12">
-                <div class="flex gap-2 sm:gap-0 flex-wrap items-center px-5 py-5 sm:py-3 border-b border-gray-200 dark:border-dark-5">
+                <div
+                    class="flex gap-2 sm:gap-0 flex-wrap items-center px-5 py-5 sm:py-3 border-b border-gray-200 dark:border-dark-5">
 
                     <div class="breadcrumb mr-auto hidden sm:flex">
-                        <a href="">Wallet</a><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                        <a href="" class="">Configuration</a><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                        <a href="">Wallet</a><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                        <a href="" class="">Configuration</a><svg xmlns="http://www.w3.org/2000/svg"
+                            width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
                         <a href="" class="breadcrumb--active"> Create Master Account</a>
                     </div>
                 </div>
                 <div class="p-5">
-                    <form action="{{ route('dashboard.wallet.master-account.store') }}" method="POST" enctype="multipart/form-data" >
+                    <form action="{{ route('dashboard.wallet.master-account.store') }}" method="POST"
+                        enctype="multipart/form-data">
                         @csrf
-                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-2">
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-0">
                             <div class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2">
-                                <label for="country" class="form-label sm:w-60">Country <span class="text-theme-6">*</span></label>
+                                <label for="country" class="form-label sm:w-60">Country <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6 tillselect-marging">
-                                    <select name="country" id="country" onchange="getCountry(this)" data-search="true" class="tom-select w-full @error('country') border-theme-6 @enderror" required>
+                                    <select name="country" id="country" onchange="getCountry(this)" data-search="true"
+                                        class="tom-select w-full @error('country') border-theme-6 @enderror" required>
                                         @foreach ($countries as $country)
-                                            <option value="{{ $country->id }}" @if (old('country',231) == $country->id) selected @endif> {{  $country->name }} </option>
+                                            <option value="{{ $country->id }}"
+                                                @if (old('country', 231) == $country->id) selected @endif> {{ $country->name }}
+                                            </option>
                                         @endforeach
                                     </select>
 
@@ -34,9 +49,10 @@
                                 </div>
                             </div>
                             <div class="col-span-12 md:col-span-8 xl:col-span-6 form-inline mt-2">
-                                <label for="status" class="form-label sm:w-60"> Status <span class="text-theme-6">*</span></label>
+                                <label for="status" class="form-label sm:w-60"> Status <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6 tillselect-marging">
-                                     <select name="status" id="status" data-search="true" class="tom-select w-full">
+                                    <select name="status" id="status" data-search="true" class="tom-select w-full">
                                         <option value="">Select Status</option>
                                         <option value="{{ \Kanexy\LedgerFoundation\Enums\LedgerStatus::ACTIVE }}"
                                             @if (old('status') === \Kanexy\LedgerFoundation\Enums\LedgerStatus::ACTIVE) selected @endif>
@@ -47,16 +63,19 @@
                                     </select>
 
                                     @error('status')
-                                    <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
                         </div>
-                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-2">
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-0">
                             <div class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2">
-                                <label for="account_holder_name" class="form-label sm:w-60">Account Holder Name <span class="text-theme-6">*</span></label>
+                                <label for="account_holder_name" class="form-label sm:w-60">Account Holder Name <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6">
-                                    <input id="account_holder_name" name="account_holder_name" type="text" class="form-control @error('account_holder_name') border-theme-6 @enderror" value="{{ old('account_holder_name') }}" required>
+                                    <input id="account_holder_name" name="account_holder_name" type="text"
+                                        class="form-control @error('account_holder_name') border-theme-6 @enderror"
+                                        value="{{ old('account_holder_name') }}" required>
 
                                     @error('account_holder_name')
                                         <span class="block text-theme-6 mt-2">{{ $message }}</span>
@@ -65,9 +84,12 @@
                             </div>
 
                             <div class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2">
-                                <label for="account_branch" class="form-label sm:w-60">Account Branch <span class="text-theme-6">*</span></label>
+                                <label for="account_branch" class="form-label sm:w-60">Account Branch <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6">
-                                    <input id="account_branch" name="account_branch" type="text" class="form-control @error('account_branch') border-theme-6 @enderror" value="{{ old('account_branch') }}" required>
+                                    <input id="account_branch" name="account_branch" type="text"
+                                        class="form-control @error('account_branch') border-theme-6 @enderror"
+                                        value="{{ old('account_branch') }}" required>
 
                                     @error('account_branch')
                                         <span class="block text-theme-6 mt-2">{{ $message }}</span>
@@ -76,31 +98,43 @@
                             </div>
                         </div>
 
-                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-2">
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-10 mt-0">
                             <div class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2">
-                                <label for="account_number" class="form-label sm:w-60">Account Number <span class="text-theme-6">*</span></label>
+                                <label for="account_number" class="form-label sm:w-60">Account Number <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6">
-                                    <input id="account_number" name="account_number" type="text" class="form-control @error('account_number') border-theme-6 @enderror" value="{{ old('account_number') }}" required>
+                                    <input id="account_number" name="account_number" type="text"
+                                        class="form-control @error('account_number') border-theme-6 @enderror"
+                                        value="{{ old('account_number') }}" required>
 
                                     @error('account_number')
                                         <span class="block text-theme-6 mt-2">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
-                            <div id="sort_code"  class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2">
-                                <label for="sort_code" class="form-label sm:w-60">Sort Code <span class="text-theme-6">*</span></label>
+                            <div id="sort_code"
+                                class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2">
+                                <label for="sort_code" class="form-label sm:w-60">Sort Code <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6">
-                                    <input id="sort_code" name="sort_code" type="text" class="form-control @error('sort_code') border-theme-6 @enderror" value="{{ old('sort_code') }}" >
+                                    <input id="sort_code" name="sort_code" type="text"
+                                        class="form-control @error('sort_code') border-theme-6 @enderror"
+                                        value="{{ old('sort_code') }}">
 
                                     @error('sort_code')
                                         <span class="block text-theme-6 mt-2">{{ $message }}</span>
                                     @enderror
                                 </div>
                             </div>
-                            <div id="ifsc_code" class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2" style="display: none;">
-                                <label for="ifsc_code" class="form-label sm:w-60">IFSC Code <span class="text-theme-6">*</span></label>
+                            <div id="ifsc_code"
+                                class="col-span-12 md:col-span-8 lg:col-span-6 sm:col-span-8 form-inline mt-2"
+                                style="display: none;">
+                                <label for="ifsc_code" class="form-label sm:w-60">IFSC Code <span
+                                        class="text-theme-6">*</span></label>
                                 <div class="sm:w-5/6">
-                                    <input id="ifsc_code" name="ifsc_code" type="text" class="form-control @error('ifsc_code') border-theme-6 @enderror" value="{{ old('ifsc_code') }}" >
+                                    <input id="ifsc_code" name="ifsc_code" type="text"
+                                        class="form-control @error('ifsc_code') border-theme-6 @enderror"
+                                        value="{{ old('ifsc_code') }}">
 
                                     @error('ifsc_code')
                                         <span class="block text-theme-6 mt-2">{{ $message }}</span>
@@ -112,7 +146,8 @@
 
 
                         <div class="text-right mt-5">
-                            <a href="{{ route('dashboard.wallet.master-account.index') }}" class="btn btn-secondary w-24 inline-block mr-1">Cancel</a>
+                            <a href="{{ route('dashboard.wallet.master-account.index') }}"
+                                class="btn btn-secondary w-24 inline-block mr-1">Cancel</a>
                             <button type="submit" class="btn btn-primary w-24">Create</button>
                         </div>
                     </form>
@@ -125,16 +160,14 @@
 @endsection
 @push('scripts')
     <script>
-        function getCountry(the)
-        {
+        function getCountry(the) {
             var country = $(the).val();
-            if(country == 231)
-            {
-                $('#sort_code').css('display','flex');
-                $('#ifsc_code').css('display','none');
-            }else{
-                $('#sort_code').css('display','none');
-                $('#ifsc_code').css('display','flex');
+            if (country == 231) {
+                $('#sort_code').css('display', 'flex');
+                $('#ifsc_code').css('display', 'none');
+            } else {
+                $('#sort_code').css('display', 'none');
+                $('#ifsc_code').css('display', 'flex');
             }
         }
     </script>
