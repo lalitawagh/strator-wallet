d3dba1f4a69dc0c3bfb19070287ac170846ba504
vivek <vivek@kanexy.com>
2022-08-30 17:17:47 +0530

    scroll fix

--- resources/views/commodity-type/index.blade.php
+++ resources/views/commodity-type/index.blade.php
@@ -8,21 +8,22 @@
             <!-- BEGIN: Daily Sales -->
             <div class="intro-y box col-span-12 xxl:col-span-12">
                 <div
-                    class="sm:flex gap-2 sm:gap-0 flex-wrap items-center px-5 py-5 sm:py-3 border-b border-gray-200 dark:border-dark-5">
+                    class="overflow-x-auto overflow-y-hidden sm:flex gap-2 gap-2 flex-wrap items-center px-5 py-5 sm:py-3 border-b border-gray-200 dark:border-dark-5 text-right">
 
-                    <div class="breadcrumb mr-auto hidden sm:flex">
-                        <a href="">Wallet</a><svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon">
-                            <polyline points="9 18 15 12 9 6"></polyline>
-                        </svg>
-                        <a href="" class="">Configuration</a><svg xmlns="http://www.w3.org/2000/svg"
+                    <div class="breadcrumb mr-auto sm:flex justify-around">
+                        <a class="whitespace-nowrap text-left" href="">Wallet</a><svg xmlns="http://www.w3.org/2000/svg"
                             width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                             class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon">
                             <polyline points="9 18 15 12 9 6"></polyline>
                         </svg>
-                        <a href="" class="breadcrumb--active">Commodity Type</a>
+                        <a href="whitespace-nowrap text-left " class="">Configuration</a><svg
+                            xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-chevron-right breadcrumb__icon breadcrumb__icon">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                        <a class="whitespace-nowrap text-left " href="" class="breadcrumb--active">Commodity Type</a>
                     </div>
                     <div>
                         <a href="{{ route('dashboard.wallet.commodity-type.create') }}"
