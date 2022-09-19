c195c35fa71aa08c09f7088d7b09957b31a7d321
vivek <vivek@kanexy.com>
2022-09-19 17:39:55 +0530

    asset type issue fixed

--- resources/views/asset-type/index.blade.php
+++ resources/views/asset-type/index.blade.php
@@ -8,7 +8,7 @@
             <div class="intro-y box col-span-12 xxl:col-span-12">
                 <div
                     class="overflow-x-auto overflow-y-hidden sm:flex gap-2 gap-2 flex-wrap items-center px-5 py-5 sm:py-3 border-b border-gray-200 dark:border-dark-5 text-right">
-                    <div class="bbreadcrumb mr-auto sm:flex justify-around">
+                    <div class="breadcrumb mr-auto sm:flex justify-around">
                         <a class="whitespace-nowrap text-left " href="">Wallet</a><svg
                             xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none"
                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -143,34 +143,34 @@
                                                         <ul class="dropdown-content">
                                                             @can(\Kanexy\LedgerFoundation\Policies\AssetTypePolicy::EDIT,
                                                                 \Kanexy\LedgerFoundation\Contracts\AssetTypeConfiguration::class)
-                                                            <li>
-                                                                <a href="{{ route('dashboard.wallet.asset-type.edit', $asset_type_list['id']) }}"
-                                                                    class="flex items-center block dropdown-item flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">
-                                                                    <i data-lucide="edit-2" class="w-4 h-4 mr-2"></i> Edit
-                                                                </a>
-                                                            </li>
+                                                                <li>
+                                                                    <a href="{{ route('dashboard.wallet.asset-type.edit', $asset_type_list['id']) }}"
+                                                                        class="flex items-center block dropdown-item flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">
+                                                                        <i data-lucide="edit-2" class="w-4 h-4 mr-2"></i> Edit
+                                                                    </a>
+                                                                </li>
                                                             @endcan
                                                             @can(\Kanexy\LedgerFoundation\Policies\AssetTypePolicy::DELETE,
                                                                 \Kanexy\LedgerFoundation\Contracts\AssetTypeConfiguration::class)
-                                                            <li>
-                                                                <form
-                                                                    action="{{ route('dashboard.wallet.asset-type.destroy', $asset_type_list['id']) }}"
-                                                                    method="POST">
-                                                                    @csrf
-                                                                    @method('DELETE')
+                                                                <li>
+                                                                    <form
+                                                                        action="{{ route('dashboard.wallet.asset-type.destroy', $asset_type_list['id']) }}"
+                                                                        method="POST">
+                                                                        @csrf
+                                                                        @method('DELETE')
 
-                                                                    <input type="hidden" name="count"
-                                                                        value="{{ $asset_type_lists->count() }}" />
-                                                                    <input type="hidden" name="previousPage"
-                                                                        value="{{ $asset_type_lists->previousPageUrl() }}" />
+                                                                        <input type="hidden" name="count"
+                                                                            value="{{ $asset_type_lists->count() }}" />
+                                                                        <input type="hidden" name="previousPage"
+                                                                            value="{{ $asset_type_lists->previousPageUrl() }}" />
 
-                                                                    <button type="submit"
-                                                                        class="w-full flex items-center block  hover:bg-red-200 dropdown-item flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">
-                                                                        <i data-lucide="trash" class="w-4 h-4 mr-2"></i>
-                                                                        Delete
-                                                                    </button>
-                                                                </form>
-                                                            </li>
+                                                                        <button type="submit"
+                                                                            class="w-full flex items-center block  hover:bg-red-200 dropdown-item flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">
+                                                                            <i data-lucide="trash" class="w-4 h-4 mr-2"></i>
+                                                                            Delete
+                                                                        </button>
+                                                                    </form>
+                                                                </li>
                                                             @endcan
                                                         </ul>
                                                     </div>
