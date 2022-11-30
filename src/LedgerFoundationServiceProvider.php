98f580cd71c32221817ba9b7a832872ef01e8243
Lalita Wagh <lalita@kanexy.com>
2022-11-30 22:13:28 +0530

    updated changes

--- src/LedgerFoundationServiceProvider.php
+++ src/LedgerFoundationServiceProvider.php
@@ -135,7 +135,7 @@ class LedgerFoundationServiceProvider extends PackageServiceProvider
         \Kanexy\Cms\Facades\RegistrationStep::addItem(new WalletRegistrationStep());
 
         \Kanexy\Cms\Facades\Cms::setRegistrationFlow(function (User $user) {
-            if ($user->is_banking_user != 0) {
+            if ($user->is_banking_user == 0) {
                 $type = 'wallet_flow';
                 return $type;
             }
@@ -144,7 +144,7 @@ class LedgerFoundationServiceProvider extends PackageServiceProvider
 
 
         \Kanexy\Cms\Facades\Cms::setRedirectRouteAfterRegistrationVerification(function (Request $request, User $user) {
-            if ($user->is_banking_user != 0) {
+            if ($user->is_banking_user == 0) {
                 return route("customer.signup.wallet.create");
             }
 
