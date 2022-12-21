cbc05c25383194641910edf2c7881bc9cdfc590e
mohamed ijazudeen <ijaz@kanexy.com>
2022-12-21 15:51:48 +0530

    Bug Resolved

--- src/Http/Controllers/Wallet/PayoutController.php
+++ src/Http/Controllers/Wallet/PayoutController.php
@@ -3,11 +3,13 @@
 namespace Kanexy\LedgerFoundation\Http\Controllers\Wallet;
 
 use App\Models\User;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
 use Kanexy\Cms\Controllers\Controller;
+use Kanexy\Cms\Helper as CmsHelper;
 use Kanexy\Cms\I18N\Models\Country;
 use Kanexy\Cms\Notifications\SmsOneTimePasswordNotification;
 use Kanexy\Cms\Setting\Models\Setting;
@@ -70,11 +72,22 @@ class PayoutController extends Controller
         $data = $request->validated();
         $user = Auth::user();
         $sender_wallet = Wallet::with('ledger')->find($data['wallet']);
-
         $receiver_ledger = Wallet::find($data['receiver_currency']);
         $beneficiary = Contact::find($data['beneficiary']);
-        $beneficiary_user = User::wherePhone($beneficiary?->mobile)->first();
+        if (is_null($beneficiary)) {
+            $data['phone'] = $user->phone;
+            $contact['mobile'] = CmsHelper::normalizePhone($user->phone);
+            $contact['workspace_id'] = $data['workspace_id'];
+            $contact['ref_type'] = 'wallet';
+            $contact['classification'] = ['beneficiary'];
+            $contact['status'] = 'active';
+            $contact['meta'] = ['country_code' => $data['country_code']];
 
+            /** @var Contact $contact */
+            $contact = Contact::create($contact);
+        }
+        $beneficiary_user = User::wherePhone($beneficiary?->mobile)->first();
+        // dd($data, $beneficiary, $beneficiary_user);
         if ($sender_wallet->id == $receiver_ledger->id && $beneficiary_user->getKey() == $user->getKey()) {
             return back()->withError("Payout not process with same wallet");
         }
