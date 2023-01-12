7303e03b37c8106d0e3bc03c21f370c5f2d68aa7
Mohamed Ijazudeen <ijaz@kanexy.com>
2023-01-12 11:04:05 +0530

    Beneficiary bug resolved

---
+++ resources/views/beneficiaries/edit.blade.php
@@ -0,0 +1,321 @@
+@extends('cms::dashboard.layouts.default')
+
+@section('title', 'Edit Beneficiary')
+
+@section('content')
+    <div class="grid grid-cols-12 gap-6">
+        <div class="col-span-12">
+            <div class="box">
+                <div class="flex items-center py-2 px-3 border-b border-gray-200 dark:border-dark-5">
+                    <h2 class="font-medium text-base mr-auto">
+                        Edit Beneficiary
+                    </h2>
+                </div>
+
+                <div class="p-5">
+                    <form action="{{ route('dashboard.banking.beneficiaries.update', $beneficiary->id) }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-8 mt-0 mb-3">
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label class="form-label sm:w-30">Contact Type <span class="text-theme-6">*</span></label>
+                                <div class="sm:w-5/6">
+                                    <div class="flex sm:flex-col sm:flex-row mt-0">
+                                        <div class="form-check mr-2">
+                                            <input id="type-personal" class="form-check-input contact-type" type="radio"
+                                                name="type" value="personal"
+                                                @if (old('type', $beneficiary->type) == 'personal') checked @endif>
+                                            <label class="form-check-label" for="type-personal">Personal</label>
+                                        </div>
+
+                                        <div class="form-check mr-2 mt-0 sm:mt-0">
+                                            <input id="type-company" class="form-check-input contact-type" type="radio"
+                                                name="type" value="company"
+                                                @if (old('type', $beneficiary->type) == 'company' || old('type') == 'company') checked @endif>
+                                            <label class="form-check-label" for="type-company">Company</label>
+                                        </div>
+                                    </div>
+
+                                    @error('type')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-8 mt-0 contact-personal">
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="first_name" class="form-label sm:w-30">First Name <span
+                                        class="text-theme-6">*</span></label>
+                                <div class="sm:w-5/6">
+                                    <input id="first_name" name="first_name" type="text"
+                                        class="form-control @error('first_name') border-theme-6 @enderror"
+                                        value="{{ old('first_name', $beneficiary->first_name) }}" required>
+
+                                    @error('first_name')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="middle_name" class="form-label sm:w-30">Middle Name</label>
+                                <div class="sm:w-5/6">
+                                    <input id="middle_name" name="middle_name" type="text"
+                                        class="form-control @error('middle_name') border-theme-6 @enderror"
+                                        value="{{ old('middle_name', $beneficiary->middle_name) }}">
+
+                                    @error('middle_name')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-8 mt-0 contact-personal">
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="last_name" class="form-label sm:w-30">Last Name <span
+                                        class="text-theme-6">*</span></label>
+                                <div class="sm:w-5/6">
+                                    <input id="last_name" name="last_name" type="text"
+                                        class="form-control @error('last_name') border-theme-6 @enderror"
+                                        value="{{ old('last_name', $beneficiary->last_name) }}" required>
+
+                                    @error('last_name')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="email" class="form-label sm:w-30">Email Address</label>
+                                <div class="sm:w-5/6">
+                                    <input id="email" name="email" type="email"
+                                        class="form-control @error('email') border-theme-6 @enderror"
+                                        value="{{ old('email', $beneficiary->email) }}">
+
+                                    @error('email')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-8 mt-0 contact-company hidden">
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="company_name" class="form-label sm:w-30">Company Name <span
+                                        class="text-theme-6">*</span></label>
+                                <div class="sm:w-5/6">
+                                    <input id="company_name" name="company_name" type="text"
+                                        class="form-control @error('company_name') border-theme-6 @enderror"
+                                        value="{{ old('company_name', $beneficiary->display_name) }}">
+
+                                    @error('company_name')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="email" class="form-label sm:w-30">Email Address</label>
+                                <div class="sm:w-5/6">
+                                    <input id="email" name="email" type="email"
+                                        class="form-control @error('email') border-theme-6 @enderror"
+                                        value="{{ old('email', $beneficiary->email) }}">
+
+                                    @error('email')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-8 mt-0">
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="landline" class="form-label sm:w-30">Landline No.</label>
+                                <div class="sm:w-5/6">
+                                    <input id="landline" name="landline" type="text"
+                                        class="form-control @error('landline') border-theme-6 @enderror"
+                                        value="{{ old('landline', $beneficiary->landline) }}">
+
+                                    @error('landline')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="mobile" class="form-label sm:w-30">Mobile No.</label>
+                                <div class="sm:w-5/6">
+                                    <input id="mobile" name="mobile" type="text"
+                                        class="form-control @error('mobile') border-theme-6 @enderror"
+                                        value="{{ old('mobile', $beneficiary->mobile) }}"
+                                        onKeyPress="if(this.value.length==11) return false;">
+
+                                    @error('mobile')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-8 mt-0">
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="bank_account_name" class="form-label sm:w-30"> Account Name <span
+                                        class="text-theme-6">*</span></label>
+                                <div class="sm:w-5/6">
+                                    <input id="bank_account_name" name="meta[bank_account_name]" type="text"
+                                        class="form-control @error('meta.bank_account_name') border-theme-6 @enderror"
+                                        value="{{ old('meta.bank_account_name', $beneficiary->meta['bank_account_name']) }}"
+                                        required disabled>
+
+                                    @error('meta.bank_account_name')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="bank_account_number" class="form-label sm:w-30"> Account No. <span
+                                        class="text-theme-6">*</span></label>
+                                <div class="sm:w-5/6">
+                                    <input id="bank_account_number" name="meta[bank_account_number]" type="text"
+                                        class="form-control @error('meta.bank_account_number') border-theme-6 @enderror"
+                                        value="{{ old('meta.bank_account_number', @$beneficiary->meta['bank_account_number']) }}"
+                                        required disabled>
+
+                                    @error('meta.bank_account_number')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-8 mt-0">
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="bank_code" class="form-label sm:w-30"> Sort Code <span
+                                        class="text-theme-6">*</span></label>
+                                <div class="sm:w-5/6">
+                                    <input id="bank_code" name="meta[bank_code]" type="text"
+                                        class="form-control @error('meta.bank_code') border-theme-6 @enderror"
+                                        value="{{ old('meta.bank_code', @$beneficiary->meta['bank_code']) }}" required
+                                        disabled>
+
+                                    @error('meta.bank_code')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="avatar" class="form-label sm:w-30">IFSC Code / IBAN</label>
+                                <div class="sm:w-5/6">
+
+                                    <input id="iban_number" name="meta[iban_number]" type="text"
+                                        class="form-control @error('meta.iban_number') border-theme-6 @enderror"
+                                        value="{{ old('meta.iban_number', @$beneficiary->meta['iban_number']) }}" required
+                                        disabled>
+
+                                    @error('iban_number')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="grid grid-cols-12 md:gap-0 lg:gap-3 xl:gap-8 mt-0">
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="avatar" class="form-label sm:w-30">Avatar</label>
+                                <div class="sm:w-5/6">
+
+                                    <input id="avatar" name="avatar" type="file"
+                                        class="form-control @error('avatar') border-theme-6 @enderror"
+                                        value="{{ old('avatar') }}">
+
+                                    @error('avatar')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-span-12 md:col-span-6 form-inline mt-2">
+                                <label for="bank_country" class="form-label sm:w-30"> Country <span
+                                        class="text-theme-6">*</span></label>
+                                <div class="sm:w-5/6">
+
+                                    {{-- <select id="bank_country" name="meta[bank_country]" data-search="true" class="tom-select form-control w-full @error('meta.bank_country') border-theme-6 @enderror" disabled>
+                                    @foreach ($countries as $country)
+                                        <option value="{{ $country->getKey() }}" @if ($country->getKey() == old('meta.bank_country', $beneficiary->meta['bank_country'])) selected @endif>{{ $country->name }}</option>
+                                @endforeach
+                                </select> --}}
+
+                                    @php $country_name = '' @endphp
+                                    @foreach ($countries as $country)
+                                        @if ($country->getKey() == @$beneficiary->meta['bank_country'])
+                                            @php $country_name = $country->name @endphp
+                                        @endif
+                                    @endforeach
+
+                                    <input id="bank_country" name="meta[bank_country]" type="text"
+                                        class="form-control @error('meta.bank_country') border-theme-6 @enderror"
+                                        value="{{ $country_name }}" required disabled>
+
+                                    @error('meta.bank_country')
+                                        <span class="block text-theme-6 mt-2">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="text-right mt-5">
+                            <a id="BeneficiaryEditeCancel"
+                                href="{{ route('dashboard.banking.beneficiaries.index', ['filter' => ['workspace_id' => $beneficiary->workspace_id]]) }}"
+                                class="btn btn-secondary w-24 inline-block mr-1">Cancel</a>
+                            <button id="BeneficiaryUpdate" type="submit" class="btn btn-primary w-24">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script>
+        function contactTypeChange(val) {
+            if (val == "{{ \Kanexy\PartnerFoundation\Banking\Enums\ContactType::COMPANY }}") {
+                $(".contact-company").removeClass('hidden hiddenform');
+                $(".contact-company").addClass('visible');
+                $(".contact-company #company_name").attr('required', 'required');
+                $("#first_name, #middle_name, #last_name").val('');
+
+                $(".contact-personal").removeClass('visible');
+                $(".contact-personal").addClass('hidden hiddenform');
+                $(".contact-personal #first_name, #last_name").removeAttr('required');
+            } else {
+                $(".contact-company").removeClass('visible');
+                $(".contact-company").addClass('hidden hiddenform');
+                $(".contact-company #company_name").removeAttr('required');
+
+
+
+                $(".contact-personal").removeClass('hidden hiddenform');
+                $(".contact-personal").addClass('visible');
+                $(".contact-personal #first_name, #last_name").attr('required', 'required');
+                $(".contact-personal #middle_name").removeAttr('required');
+                $(".contact-personal #email").removeAttr('required');
+                $("#company_name").val('');
+            }
+        }
+        $(".contact-type").each(function() {
+            if ($(this).is(':checked')) {
+                contactTypeChange($(this).val());
+            }
+        });
+
+        $(".contact-type").click(function() {
+            contactTypeChange($(this).val());
+        });
+    </script>
+@endpush
---
+++ src/Http/Controllers/WalletBeneficiaryController.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Kanexy\PartnerFoundation\Banking\Controllers;
+
+use Illuminate\Http\Request;
+use Kanexy\Cms\Controllers\Controller;
+use Kanexy\Cms\Helper;
+use Kanexy\Cms\I18N\Models\Country;
+use Kanexy\Cms\Notifications\SmsOneTimePasswordNotification;
+use Kanexy\Cms\Setting\Models\Setting;
+use Kanexy\PartnerFoundation\Banking\Models\Account;
+use Kanexy\PartnerFoundation\Banking\Requests\StoreBeneficiaryRequest;
+use Kanexy\PartnerFoundation\Banking\Requests\UpdateBeneficiaryRequest;
+use Kanexy\PartnerFoundation\Core\Dtos\CreateBeneficiaryDto;
+use Kanexy\PartnerFoundation\Core\Services\WrappexService;
+use Kanexy\PartnerFoundation\Cxrm\Events\ContactCreated;
+use Kanexy\PartnerFoundation\Cxrm\Events\ContactDeleted;
+use Kanexy\PartnerFoundation\Cxrm\Events\ContactDeleting;
+use Kanexy\PartnerFoundation\Cxrm\Models\Contact;
+use Kanexy\PartnerFoundation\Cxrm\Policies\ContactPolicy;
+use Kanexy\PartnerFoundation\Workspace\Models\Workspace;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class WalletBeneficiaryController extends Controller
+{
+    private WrappexService $service;
+
+    public function __construct(WrappexService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(Request $request)
+    {
+        $this->authorize(ContactPolicy::INDEX, Contact::class);
+
+        $contacts = QueryBuilder::for(Contact::class)
+            ->allowedFilters([
+                AllowedFilter::exact('workspace_id'),
+            ]);
+
+        $workspace = null;
+
+        if ($request->has('filter.workspace_id')) {
+            $workspace = Workspace::findOrFail($request->input('filter.workspace_id'));
+        }
+
+        $beneficiaries = $contacts->beneficiaries()->where('ref_type', 'wallet')->verified()->latest()->paginate();
+
+
+        return view("ledger-foundation::beneficiaries.index", compact('beneficiaries', 'workspace'));
+    }
+
+    public function create(Request $request)
+    {
+        $this->authorize(ContactPolicy::CREATE, Contact::class);
+
+        $workspace = Workspace::findOrFail($request->input('workspace_id'));
+
+        $countries = Country::get();
+        $defaultCountry = Setting::getValue('default_country');
+
+        $accounts = Account::whereNotNull('account_number')->latest()->get(['id', 'name', 'account_number']);
+
+        return view("partner-foundation::banking.beneficiaries.create", compact('countries', 'defaultCountry', 'workspace', 'accounts'));
+    }
+
+    public function store(StoreBeneficiaryRequest $request)
+    {
+        $data = $request->validated();
+
+        $workspace = Workspace::findOrFail($request->input('workspace_id'));
+
+        if ($data['type'] == 'company') {
+            $data['display_name'] = Helper::removeExtraSpace($data['company_name']);
+        } else {
+            $data['display_name'] = Helper::removeExtraSpace(implode(' ', [$data['first_name'], $data['middle_name'], $data['last_name']]));
+        }
+
+        if ($request->hasFile('avatar')) {
+            $data['avatar'] = $request->file('avatar')->store('Images', 'azure');
+        }
+
+        $beneficiaryRefId = $this->service->createBeneficiary(
+            new CreateBeneficiaryDto($workspace->ref_id, $data)
+        );
+
+        $data['workspace_id'] = $workspace->id;
+        $data['ref_id']       = $beneficiaryRefId;
+        $data['ref_type']     = 'wallet';
+
+
+        /** @var Contact $contact */
+        $contact = Contact::create($data);
+
+        event(new ContactCreated($contact));
+
+        /** @var \App\Models\User $user */
+        $user = auth()->user();
+        //$user->notify(new EmailOneTimePasswordNotification($contact->generateOtp("email")));
+        if (config('services.disable_sms_service') == false) {
+            $user->notify(new SmsOneTimePasswordNotification($contact->generateOtp("sms")));
+        } else {
+            $contact->generateOtp("sms");
+        }
+
+        if (request()->has('callback_url') && request()->input('callback_url')) {
+            return $contact->redirectForVerification(request()->input('callback_url'), 'sms');
+        }
+
+        return $contact->redirectForVerification(route('dashboard.banking.beneficiaries.index', ['filter' => ['workspace_id' => $workspace->id]]), 'sms');
+    }
+
+    public function edit(Contact $beneficiary)
+    {
+        $this->authorize(ContactPolicy::EDIT, $beneficiary);
+
+        $countries = Country::get();
+        $defaultCountry = Setting::getValue('default_country');
+
+        return view("partner-foundation::banking.beneficiaries.edit", compact('beneficiary', 'countries', 'defaultCountry'));
+    }
+
+    public function update(UpdateBeneficiaryRequest $request, Contact $beneficiary)
+    {
+        $data = $request->validated();
+
+        if ($data['type'] == 'company') {
+            $data['display_name'] = Helper::removeExtraSpace($data['company_name']);
+        } else {
+            $data['display_name'] = Helper::removeExtraSpace(implode(' ', [$data['first_name'], $data['middle_name'], $data['last_name']]));
+        }
+
+        if ($request->hasFile('avatar')) {
+            $data['avatar'] = $request->file('avatar')->store('Images', 'azure');
+        }
+
+        $beneficiary->update($data);
+
+        return redirect()->route("dashboard.banking.beneficiaries.index", ['filter' => ['workspace_id' => $beneficiary->workspace_id]])->with([
+            'status' => 'success',
+            'message' => 'The beneficiary updated successfully.',
+        ]);
+    }
+
+    public function destroy(Contact $beneficiary)
+    {
+        $this->authorize(ContactPolicy::DELETE, $beneficiary);
+
+        event(new ContactDeleting($beneficiary));
+
+        $beneficiary->delete();
+
+        event(new ContactDeleted($beneficiary));
+
+        return redirect()->route("dashboard.banking.beneficiaries.index", ['filter' => ['workspace_id' => $beneficiary->workspace_id]])->with([
+            'status' => 'success',
+            'message' => 'The beneficiary deleted successfully.',
+        ]);
+    }
+
+    public function getPartnerAccount(Request $request)
+    {
+        $account_id = $request->input('account_id') ?? '';
+
+        if (!empty($account_id)) {
+            $account_details = Account::with('workspaces')->findOrFail($account_id);
+        }
+
+        return $account_details->toArray();
+    }
+}
